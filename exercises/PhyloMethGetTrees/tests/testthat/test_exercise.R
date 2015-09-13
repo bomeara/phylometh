95ebb7aadd177cfe151965b30f3fec36673636c8
Brian O'Meara <omeara.brian@gmail.com>
2015-09-12 21:32:20 -0400

    passing correct function name to test

--- exercises/PhyloMethGetTrees/tests/testthat/test_exercise.R
+++ exercises/PhyloMethGetTrees/tests/testthat/test_exercise.R
@@ -2,8 +2,8 @@ test_that("GetFromTreeBase_exercise",{
 	expect_is(GetFromTreeBase_exercise(), "phylo")
 })
 
-test_that("GetFromOpenTree_exercise",{
-	opentree.result <- GetFromOpenTree_exercise()
+test_that("GetTreeFromOpenTree_exercise",{
+	opentree.result <- GetTreeFromOpenTree_exercise()
 	expect_is(opentree.result[[1]], "phylo")
 	expect_is(opentree.result[[2]], "phylo")
 })
--- exercises/PhyloMethGetTrees/tests/testthat/test_solution.R
+++ exercises/PhyloMethGetTrees/tests/testthat/test_solution.R
@@ -2,8 +2,8 @@ test_that("GetFromTreeBase_solution",{
 	expect_is(GetFromTreeBase_solution(), "phylo")
 })
 
-test_that("GetFromOpenTree_solution",{
-	opentree.result <- GetFromOpenTree_solution()
+test_that("GetTreeFromOpenTree_solution",{
+	opentree.result <- GetTreeFromOpenTree_solution()
 	expect_is(opentree.result[[1]], "phylo")
 	expect_is(opentree.result[[2]], "phylo")
 })
