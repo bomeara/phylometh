df9788e98c0d08dbf03b5dbc4cb94236b77353ea
Brian O'Meara <omeara.brian@gmail.com>
2015-09-12 20:09:26 -0400

    first round of testing

--- exercises/PhyloMeth_GetTrees/R/solutions.R
+++ exercises/PhyloMeth_GetTrees/R/solutions.R
@@ -5,18 +5,23 @@
 # Another issue is that until recenty TreeBASE prohibited branch length
 # information, though more recent trees do (often) have this info. 
 GetFromTreeBase_solution <- function() {
-	# Get all trees for Formica, searching by taxon, with branch lengths
+	# Get all trees for Formica (a genus of ants), searching by taxon, with branch lengths
 	# Replace the __________ with appropriate arguments
 	# When you run this, it'll take some time. But less time than sequencing
 	# and building the tree yourself.
-#	trees <- search_treebase(input="__________ by="__________", branch_lengths=__________)
+#	trees <- search_treebase(input="__________", by="__________", branch_lengths=__________)
 	trees <- search_treebase(input="Formica", by="taxon", branch_lengths=TRUE)
 	
 	# Pull out just the biggest tree (for convenience)
 	biggest.tree <- trees[[which.max(sapply(trees, Ntip))]]
 	
-	# Get the metadata (data describing the data -- authors, etc.)
-	biggest.tree.metadata <- metadata(biggest.tree$S.id)
+	# Print out a description of the biggest tree
+#	print(__________)
+	print(biggest.tree)
 	
+	# Print let's look at the TreeBASE page for this study
+	browseURL(paste("http://treebase.org/treebase-web/search/study/summary.html?id=", biggest.tree$S.id, sep=""))
 	
+	# Return this tree
+	return(biggest.tree)
 }
