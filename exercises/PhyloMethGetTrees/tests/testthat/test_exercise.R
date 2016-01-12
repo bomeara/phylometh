f1bacef9cae99ebee30a85b1749b84db3b4f1111
Brian OMeara <omeara.brian@gmail.com>
2016-01-12 15:26:02 -0500

    don't need exercises, make as separate repos for classroom

--- exercises/PhyloMethGetTrees/tests/testthat/test_exercise.R
+++
@@ -1,9 +0,0 @@
-test_that("GetFromTreeBase_exercise",{
-	expect_is(GetFromTreeBase_exercise(), "phylo")
-})
-
-test_that("GetTreeFromOpenTree_exercise",{
-	opentree.result <- GetTreeFromOpenTree_exercise()
-	expect_is(opentree.result[[1]], "phylo")
-	expect_is(opentree.result[[2]], "phylo")
-})
--- exercises/PhyloMethGetTrees/tests/testthat/test_solution.R
+++
@@ -1,9 +0,0 @@
-test_that("GetFromTreeBase_solution",{
-	expect_is(GetFromTreeBase_solution(), "phylo")
-})
-
-test_that("GetTreeFromOpenTree_solution",{
-	opentree.result <- GetTreeFromOpenTree_solution()
-	expect_is(opentree.result[[1]], "phylo")
-	expect_is(opentree.result[[2]], "phylo")
-})
