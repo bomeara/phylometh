fab64a741bf21cb90f17767e6288db67aa8668f1
mmamantov <mmamanto@vols.utk.edu>
2017-01-17 21:01:52 -0500

    Add files via upload

--- exercises/gettingstarted/gettingstarted_exercise.R
+++ exercises/gettingstarted/gettingstarted_exercise.R
@@ -1,16 +1,15 @@
 # Some basic tests to make sure things are running.
-
 GetHowManyTrees <- function(ntaxa=10) {
-	#ape can compute this for us.
-	result <- ___SOME FUNCTION WITH AN ARGUMENT_____
-	return(result)
+  #ape can compute this for us.
+  result <- howmanytrees(ntaxa)
+  return(result)
 }
 
 GetAlignment <- function() {
-	#Let's see where mafft is installed
-	library(ape)
-	print(system("which mafft"))
-	data(woodmouse)
-	result <- ips::mafft(____SOME INPUT ARGUMENT____)
-	return(result)
+  #Let's see where mafft is installed
+  library(ape)
+  print(system("which mafft"))
+  data(woodmouse)
+  result <- ips::mafft(woodmouse, path="C:/mafft-win/mafft.bat")
+  return(result)
 }
