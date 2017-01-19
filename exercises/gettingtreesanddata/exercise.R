1fa964a76a75d7f6d5633e64eb8171d3800ea44e
Maggie Limbeck <mlimbeck@vols.utk.edu>
2017-01-19 15:04:16 -0500

    please let this work

--- exercises/gettingtreesanddata/exercise.R
+++ exercises/gettingtreesanddata/exercise.R
@@ -1,18 +1,18 @@
-# Let's get some trees from Open Tree of Life. This is in some ways a
+\# Let's get some trees from Open Tree of Life. This is in some ways a
 # successor to TreeBASE: another repository of trees (though it has only partial
 # overlap with the trees in TreeBASE (or in another repository, Dryad). Unlike
 # TreeBASE, anyone, not just the author, can add trees to Open Tree's 
 # repository. Another important distinction is that Open Tree seeks to create
 # a single tree for all life by creating essentially a supreme super tree.
-GetTreeFromOpenTree_solution <- function() {
+GetTreeFromOpenTree_solution <- function() 
 	library(rotl)
-	library(ape)
+  library(ape)
 	# Now let's look for the gecko genus Anolis
 	# Figure out the id used for Anolis in Open Tree's database
 
 	# Replace the "__________" with appropriate arguments
 	
-	anolis.id <- tnrs_match_names("__________")$ott_id
+	anolis.id <- tnrs_match_names("Anolis")$ott_id
 	
 	# Now get Open Tree's current best estimate of the phylogeny for the group
 	# They call this the tree of life; we can get the subtree for just this group.
@@ -20,6 +20,7 @@ GetTreeFromOpenTree_solution <- function() {
 	
 	# Let's plot the tree:
 	plot.phylo("__________", type="fan", cex=0.2)
+	plot.phylo(anolis.tree,type="fan",cex=0.2)
 	
 	# It has a lot of polytomies, representing uncertainty. A maximally resolved
 	# tree (if rooted) will have one fewer internal nodes than terminal nodes:
@@ -48,7 +49,7 @@ GetTreeFromOpenTree_solution <- function() {
 	# And let's get the tree from this study
 	# Look in rotl documentation for the right function
 	# Hint: get_study_trees() is close, but you don't know the tree.id
-	anolis.study1.tree1 <- get_______FUNCTION_FROM_rotl____(anolis.studies.ids[[1]])
+	anolis.study1.tree1 <- get_study(anolis.studies.ids[[1]])
 	
 	# And plot it
 	plot.phylo(anolis.study1.tree1, type="fan", cex=0.2)
@@ -57,4 +58,3 @@ GetTreeFromOpenTree_solution <- function() {
 	return.list <- list(anolis.tree, anolis.study1.tree1)
 	names(return.list) <- c("tol.tree", "study1.tree")
 	return(return.list)
-}
